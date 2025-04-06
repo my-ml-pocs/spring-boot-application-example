43d9c0d0c2c8723305ea5ce14a0d36d6fe5a168e
softengrahmed <softengrahmed@gmail.com>
2025-04-05 21:10:42 -0400

    Update src/main/java/com/myapp/controller/DriverController.java

--- src/main/java/com/myapp/controller/DriverController.java
+++ src/main/java/com/myapp/controller/DriverController.java
@@ -49,21 +49,21 @@ public class DriverController {
 
 
     @GetMapping("/{driverId}")
-    public DriverDTO getDriver(@Valid @PathVariable long driverId) throws EntityNotFoundException {
+    public DriverDTO getDriver(@Valid @PathVariable long driverId) {
         return DriverMapper.makeDriverDTO(driverService.find(driverId));
     }
 
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public DriverDTO createDriver(@Valid @RequestBody DriverDTO driverDTO) throws ConstraintsViolationException {
+    public DriverDTO createDriver(@Valid @RequestBody DriverDTO driverDTO) {
         DriverDO driverDO = DriverMapper.makeDriverDO(driverDTO);
         return DriverMapper.makeDriverDTO(driverService.create(driverDO));
     }
 
 
     @DeleteMapping("/{driverId}")
-    public void deleteDriver(@Valid @PathVariable long driverId) throws EntityNotFoundException {
+    public void deleteDriver(@Valid @PathVariable long driverId) {
         driverService.delete(driverId);
     }
 
